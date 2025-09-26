bed3148edef5059588f1836f6d0e5c8e33d8e2be
TheSardunya <jellor883@proton.me>
2025-09-26 20:26:53 +0300

    Delete oreno.h

--- oreno.h
+++
@@ -1,8 +0,0 @@
-#ifndef ORENO_H
-#define ORENO_H
-std::string clean_white_space(std::string a);
-std::wstring clean_white_space(std::wstring a);
-void clean_null(std::string a);
-bool TryParse(std::string rawx);
-bool TryParse(std::wstring rawx);
-#endif
